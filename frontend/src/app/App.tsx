1517b3010d64afd858d60ef34c0d88d53b125c09
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-09-25 22:46:30 +0200

    remove prettier, use eslint for formatting only

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import React from 'react'
+import logo from '../logo.svg'
+import './App.css'
 
-function App(): any {
+function App (): any {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +15,7 @@ function App(): any {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
