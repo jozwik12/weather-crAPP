60eda579bce0b32dc6b71cc99813a94d4072137a
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-09-26 23:48:06 +0200

    remove CRA clutter, mock main page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import logo from '../logo.svg'
 import './App.css'
+import logo from '../assets/mock_title_icon.png'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="main-container">
+      <h1>Homepage title</h1>
+      <h2>Weather is nice today</h2>
+      <header className="display-body">
+        <img src={logo} className="placeholder-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This the weather crApp
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
       </header>
     </div>
   )
