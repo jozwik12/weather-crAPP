3389a4e9a4a694f2ec437b59e559ad2464242aea
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-09-28 00:51:23 +0200

    show error message on error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 import { paths } from './pagesConfig'
 
 function ErrorPage (): JSX.Element {
+  const error: any = useRouteError()
   return (
     <div>
       <h1>Error Page</h1>
       <p>Oh no!</p>
       <p>Something went wrong!</p>
+      <p>
+        <i>{error.error.message}</i>
+      </p>
       <Link to={paths.homePagePath}>Go back to homepage</Link>
     </div>
   )
