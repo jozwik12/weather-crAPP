ce014dc815e5b160fd92bd7e71efe9b99abfe4ca
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-09-30 23:14:09 +0200

    add dark mode switch

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { paths } from '../pages/pagesConfig'
+import { Form } from 'react-bootstrap'
 
 function NavigationBar (): React.ReactNode {
   return (
@@ -8,6 +9,7 @@ function NavigationBar (): React.ReactNode {
       <Link to={paths.homePagePath}>Home Page</Link>
       <Link to={paths.weatherDetailsPagePath}>Details Page</Link>
       <Link to={paths.aboutPage}>About Page</Link>
+      <Form.Switch type="switch" id="darkModeSwitch" label="Dark mode" />
     </div>
   )
 }
