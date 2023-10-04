54403b9bb3ac4780d96819d23eaff655a47e781a
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-10-04 22:05:41 +0200

    move reducers to separate directory

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,7 +2,7 @@ import {
   configureStore,
   type Store
 } from '@reduxjs/toolkit'
-import rootReducer from './reducers'
+import rootReducer from './reducers/reducersIndex'
 
 const store: Store = configureStore({
   reducer: rootReducer
