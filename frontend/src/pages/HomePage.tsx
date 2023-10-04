a22f3bbf1062a9fade1edf549e6090176882333b
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-10-04 23:12:11 +0200

    change app styling depending on dark mode

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import logo from '../assets/mock_title_icon.png'
 import '../styles/HomePage.scss'
+import { useSelector } from 'react-redux'
+import { type RootState } from '../app/store'
 
 function HomePage (): React.ReactNode {
+  const isDarkModeEnabled: boolean = useSelector((state: RootState) => state.app.isDarkModeEnabled)
+
   return (
     <div className="main-container">
-      <h1>Homepage title</h1>
-      <h2>Weather is nice today</h2>
+      <div className={`page-header-${isDarkModeEnabled ? 'dark' : 'light'}`}>
+        <h1>Homepage title</h1>
+        <h2>Weather is nice today</h2>
+      </div>
       <header className="display-body">
         <img src={logo} className="placeholder-logo" alt="logo" />
         <p>
