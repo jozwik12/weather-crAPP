2ab123947f4edf0ffbc78d6384f089263abf7c3d
Szymon Józwicki <szymonjozwicki@gmail.com>
2023-10-04 22:44:23 +0200

    link the mock switch to actually switch dark mode state

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -2,14 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { paths } from '../pages/pagesConfig'
 import { Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleDarkMode } from '../app/reducers/appSlice'
+import { type RootState } from '../app/store'
 
 function NavigationBar (): React.ReactNode {
+  const dispatch = useDispatch()
+  const isDarkModeEnabled: boolean = useSelector((state: RootState) => state.app.isDarkModeEnabled)
+
+  const action = (): void => {
+    dispatch(toggleDarkMode(!isDarkModeEnabled))
+  }
+
   return (
     <div>
       <Link to={paths.homePagePath}>Home Page</Link>
       <Link to={paths.weatherDetailsPagePath}>Details Page</Link>
       <Link to={paths.aboutPage}>About Page</Link>
-      <Form.Switch type="switch" id="darkModeSwitch" label="Dark mode" />
+      <Form.Check
+        type="switch"
+        id="darkModeSwitch"
+        label="Dark mode"
+        onClick={action}/>
     </div>
   )
 }
